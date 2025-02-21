b60167c7d5460ef5c3a2c37609223c79d985db63
rampage1212 <>
2025-02-21 06:22:40 -0800

    Update environment

--- Calling/src/app/config/environment.ts
+++ Calling/src/app/config/environment.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 export const environment = {
-  API_ENDPOINT: 'https://celebrated-unity-production.up.railway.app/'
+  API_ENDPOINT: 'http://localhost:8080/'
 };

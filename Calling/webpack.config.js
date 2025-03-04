0a460f6845a03869b07b5672ca449c8e1f7ecad8
rampage1212 <>
2025-03-04 07:30:36 -0800

    fix

--- Calling/webpack.config.js
+++ Calling/webpack.config.js
@@ -1,10 +1,19 @@
 // Copyright (c) Microsoft Corporation.
-// Licensed under the MIT license.
+// Licensed under the MIT License.
 
 /* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
 
 module.exports = (env) => {
   const babelConfig = require('./.babelrc.js');
   const commonConfig = require('./common.webpack.config')(__dirname, env, babelConfig);
+  commonConfig.resolve = {
+    ...commonConfig.resolve,
+    fallback: {
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+      crypto: require.resolve('crypto-browserify')
+    }
+  };
   return commonConfig;
 };
